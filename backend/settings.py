1c485f32f72dc1c7546d36fe4bb53355c2392a02
LeyderBadilloValdes <leyder850@gmail.com>
2025-07-27 19:13:42 -0500

    Eliminar apps de mantenimiento y reportes

--- backend/settings.py
+++ backend/settings.py
@@ -54,9 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Apps del proyecto
-    'mantenimiento',
-    'reportes',
+
 ]
 
 MIDDLEWARE = [
